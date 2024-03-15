555c8a28952913a6f9d77a953b038ccc181d9997
yaw oppong <opp.mic@gmail.com>
2024-03-15 10:42:33 +0100

    css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,11 +3,11 @@ import SectionTitle from "./SectionTitle";
 const Skills = () => {
   return <section className="py-20 " id="skills">
     <SectionTitle text="tech stack" />
-    <div className="px-8 grid gap-y-4 mt-8 md:px-16  md:grid-cols-2 lg:grid-cols-3 lg:gap-x-64 lg:w-3/4">
+    <div className="max-w-5xl mx-auto grid px-8 gap-4  md:grid-cols-2 lg:grid-cols-3" >
       {
         skills.map((skill) => {
           const { id, title, icon, text } = skill;
-          return <article key={id} className="flex flex-col border-2 gap-y-2 shadow-md w-96 rounded-md px-4 py-4">
+          return <article key={id} className="flex flex-col border-2 gap-y-2 shadow-md w-96 rounded-md px-4 py-4 lg:w-80">
             <span >{icon}</span>
             <div className="flex flex-col mt-2">
               <h2 className="text-gray-700 text-xl -lg:mt-12">{title}</h2>
