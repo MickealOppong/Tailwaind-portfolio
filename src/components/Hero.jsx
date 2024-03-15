282615623f09ba58f7c17a4689d744c38d347ed6
yaw oppong <opp.mic@gmail.com>
2024-03-15 08:09:26 +0100

    css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () => {
   return <section className='flex justify-center font-roboto-medium'>
     <div className="mx-auto max-w-7xl py-36 px-8 grid grid-col-2 items-start gap-8 mt-16 md:items-center md:gap-y-16 md:px-16 md:flex flex-col">
       <article className='grid grid-cols-2 -rotate-3 '>
-        <div className="w-40 h-60 md:w-80 lg:w-96 bg-slate-500">
+        <div className="w-40 h-60 md:w-80 lg:w-96">
           <img src={img} />
         </div>
         <div className="flex flex-col -ml-16">
