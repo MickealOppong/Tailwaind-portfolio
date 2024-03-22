3ea3e1852f4f3d93890ce102559b678cf3935d50
yaw oppong <opp.mic@gmail.com>
2024-03-22 15:46:54 +0100

    css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = () => {
       <img src={hero} alt="" className='hidden md:flex md:w-80 md:h-56 ' />
       <div className='flex flex-col w-96'>
         <h4 className="uppercase font-bold mb-4">A bit about me</h4>
-        <p className="tracking-wider leading-6 ">
+        <p className="tracking-wider leading-6 text-sm">
           I am a graduate of MA Finance from University of Econommics in Katowice, i hold a Bachelor of laws degree. I am married and a father who enjoys creating web applications. I love what i do and i do what i love.
         </p>
-        <p className="tracking-wider leading-6 mt-4 text-base"> My journey to software development was influence by my previous work experience in a software company where i was involved in design and implementation of functional requirements for Enterprise Resource applications.</p>
+        <p className="tracking-wider leading-6 mt-4 text-base text-sm"> My journey to software development was influence by my previous work experience in a software company where i was involved in design and implementation of functional requirements for Enterprise Resource applications.</p>
       </div>
     </div>
 
