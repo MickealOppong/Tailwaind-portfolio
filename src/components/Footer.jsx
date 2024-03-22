6a985bd28161d30b106eed3446d18e814603d831
yaw oppong <opp.mic@gmail.com>
2024-03-22 14:47:03 +0100

    cd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,16 +8,16 @@ const Footer = () => {
 
     <div className="grid grid-cols-2 gap-x-24 items-center ml-16 md:flex md:flex-row md:justify-between">
       <div className="flex flex-col">
-        <p className="uppercase ">contact</p>
+        <p className="uppercase text-neutral font-bold">contact</p>
         <span>mikel.oppon@gmail.com</span>
         <span>(+48) 739-593-752</span>
       </div>
       <div className='flex gap-4 '>
-        <a href="https://github.com" referrerPolicy="no-referrer" target="_blank"><FaGithub className='text-slate-500 h-6 w-6 ' /></a>
+        <a href="https://github.com" referrerPolicy="no-referrer" target="_blank"><FaGithub className='text-neutral h-6 w-6 ' /></a>
       </div>
     </div>
     <div className="flex mt-2">
-      <p className="text-slate-500 capitalize tracking-wider font-medium mt-2 ">&#169; <span>{getCurrentYear()}</span> mike oppong.</p>
+      <p className="text-neutral capitalize tracking-wider font-medium mt-2 ">&#169; <span>{getCurrentYear()}</span> mike oppong.</p>
     </div>
 
   </footer>
