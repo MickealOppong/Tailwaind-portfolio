a7f619eaff472b81ede77c34070807994c6dd5e9
yaw oppong <opp.mic@gmail.com>
2024-03-22 15:45:36 +0100

    cdd

--- src/data.jsx
+++ src/data.jsx
@@ -2,7 +2,8 @@ import { nanoid } from "nanoid";
 import { FaCode, FaHtml5, FaJava, FaJs, FaReact } from 'react-icons/fa';
 import img from './assets/spring.png';
 export const links = [
-  { id: nanoid(), href: "https://vite-cms.netlify.app/", text: "projects" }
+  { id: nanoid(), href: "https://vite-cms.netlify.app/", text: "projects" },
+  { id: nanoid(), href: "https://github.com", text: "github" },
 ]
 
 export const techStack = [
